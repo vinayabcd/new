c03236848bc3cecb8ec779edc888b9be985f7f1a
vinayabcd <109613636+vinayabcd@users.noreply.github.com>
2024-03-13 18:52:17 +0530

    Update Division.java

--- Division.java
+++ Division.java
@@ -5,7 +5,7 @@ class Divsion {
         int[] values = { 100, 52, 43, 99, 42, 40, 91 };
         int sum = 0;
         for (int i = 0; i < values.length; i++) {
-            if (values[i] % 10 == 0) {
+            if (values[i] % 5 == 0) {
                 sum += values[i];
             }
         }
